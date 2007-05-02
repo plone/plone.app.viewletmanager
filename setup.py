ebdd21dfeee6109f3d2e2121584d933c23102e10
Florian Schulze <florian.schulze@gmx.net>
2007-05-02 23:53:33 +0000

    Added egg stuff.
    svn path=/plone.app.viewletmanager/trunk/; revision=14786

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.0b1'
+
+setup(name='plone.app.viewletmanager',
+      version=version,
+      description="configurable viewlet manager",
+      long_description="""\
+A viewlet manager which allows configuration of the filtering and ordering
+per skin.
+""",
+      classifiers=[], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Florian Schulze',
+      author_email='fschulze@plonesolutions.com',
+      url='https://svn.plone.org/svn/plone/plone.app.viewletmanager',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
