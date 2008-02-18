ba55b48527d4eb792bc9a2e7f1885900e66001d6
Wichert Akkerman <wichert@wiggy.net>
2008-02-18 09:49:23 +0000

    plone is a namespace as well
    svn path=/plone.app.viewletmanager/branches/1.1/; revision=19368

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(name='plone.app.viewletmanager',
       url='https://svn.plone.org/svn/plone/plone.app.viewletmanager',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone.app'],
+      namespace_packages = ['plone', 'plone.app'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
