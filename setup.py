b00ac8cf40a52b4c9e73d7255f83d73e51a9bc94
Maurits van Rees <maurits@vanrees.org>
2016-08-10 23:31:51 +0200

    Preparing release 2.0.10
    [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '2.0.10.dev0'
+version = '2.0.10'
 
 long_description = '{0}\n{1}'.format(
     open('README.rst').read(),
