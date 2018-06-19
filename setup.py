67c44ee42db093d0705949517500751797ceaeed
esteele <eric@esteele.net>
2018-06-19 10:11:08 -0400

    Preparing release 2.0.12
    [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '2.0.12.dev0'
+version = '2.0.12'
 
 long_description = '{0}\n{1}'.format(
     open('README.rst').read(),
