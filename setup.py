646c5959eecd8537817ad20a19d74bbf6a1e6cc4
esteele <eric@esteele.net>
2018-06-19 10:11:35 -0400

    Back to development: 2.0.13
    [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '2.0.12'
+version = '2.0.13.dev0'
 
 long_description = '{0}\n{1}'.format(
     open('README.rst').read(),
