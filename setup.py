1497288ec91655232372593760309b5a49b948db
Maurits van Rees <maurits@vanrees.org>
2018-10-01 15:36:38 +0200

    Fixed PyPI links in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     keywords='',
     author='Plone Foundation',
     author_email='plone-developers@lists.sourceforge.net',
-    url='https://pypi.python.org/pypi/plone.app.viewletmanager',
+    url='https://pypi.org/project/plone.app.viewletmanager',
     license='GPL version 2',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['plone', 'plone.app', ],
