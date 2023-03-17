2a8e40f058b3a19b70fc51e223c6bddf6f2683e9
Gil Forcada Codinachs <gil.gnome@gmail.com>
2023-03-17 23:43:17 +0100

    feat: remove last six usage

--- plone/app/viewletmanager/tests/test_docs.py
+++ plone/app/viewletmanager/tests/test_docs.py
@@ -1,8 +1,6 @@
 from plone.app.viewletmanager.testing import optionflags
 
 import doctest
-import re
-import six
 import unittest
 
 
@@ -10,17 +8,6 @@ doc_tests = [
     "storage.rst",
     "manager.rst",
 ]
-
-
-class Py23DocChecker(doctest.OutputChecker):
-    def check_output(self, want, got, optionflags):
-        want = re.sub("u'(.*?)'", "'\\1'", want)
-        got = re.sub(
-            "zope.interface.interfaces.ComponentLookupError",
-            "ComponentLookupError",
-            got,
-        )
-        return doctest.OutputChecker.check_output(self, want, got, optionflags)
 
 
 def test_suite():
@@ -31,7 +18,6 @@ def test_suite():
                 f"tests/{doc_file}",
                 package="plone.app.viewletmanager",
                 optionflags=optionflags,
-                checker=Py23DocChecker(),
             )
             for doc_file in doc_tests
         ]
